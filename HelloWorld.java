3ab27126119f15dde5b4f3a0952eeda0d12c6fc1
Shrenikrpatil <47065697+Shrenikrpatil@users.noreply.github.com>
2022-04-10 00:04:16 +0530

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,16 @@
+public class Hello 
+{
+    public static void add()
+    {
+        System.out.println("Shrenik"+i);
+    }
+    public static int i=20;
+    public static void main(String[] args) 
+    {
+
+        System.out.println("Shrenik"+i);
+        add();
+        
+    }
+
+}
